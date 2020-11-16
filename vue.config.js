5b610c5d38ef252d73805b7f4c9288782bf78988
Tomxuetao <useremail@qq.com>
2020-11-16 17:21:30 +0800

    初始化配置

---
+++ vue.config.js
@@ -0,0 +1,39 @@
+// vue.config.js
+const path = require('path')
+
+module.exports = {
+  publicPath: './',
+  filenameHashing: false,
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+    }
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    // 清除已有的所有 loader。
+    // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
+    svgRule.uses.clear()
+    svgRule
+      .test(/\.svg$/)
+      .include.add(path.resolve(__dirname, './src/icons'))
+      .end()
+      .use('svg-sprite')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: '[name]'
+      })
+    const fileRule = config.module.rule('file')
+    fileRule.uses.clear()
+    fileRule
+      .test(/\.svg$/)
+      .exclude.add(path.resolve(__dirname, './src/icons'))
+      .end()
+      .use('file-loader')
+      .loader('file-loader')
+  }
+}
